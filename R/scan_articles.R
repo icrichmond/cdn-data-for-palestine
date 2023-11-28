cf69f1409d68f058de00e25b1ef4dd7f9cd2aac3
Isabella Richmond <52459005+icrichmond@users.noreply.github.com>
2023-11-28 12:12:31 -0500

    use possibly for erroring

--- R/scan_articles.R
+++ R/scan_articles.R
@@ -1,9 +1,12 @@
 scan_articles <- function(articles){
   
+  posshtml= possibly(.f = read_html, otherwise = 'error')
+  
   t <- articles %>%
-    mutate(html = map(link, function(x){read_html(x[[1]])}),
-           content = map(html, ~ .x %>% 
-                           html_nodes("p") %>% 
+    mutate(html = map(link, ~posshtml(.x[[1]]))) %>%
+    filter(html != 'error') %>%
+    mutate(content = map(html, ~ .x %>% 
+                          html_nodes("p") %>% 
                           html_text2() %>% 
                           paste(collapse = ",")))
   
